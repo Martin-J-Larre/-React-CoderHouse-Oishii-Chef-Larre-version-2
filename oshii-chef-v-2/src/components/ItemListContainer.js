de42b23da83bd17b35887607fb7fb2b4e56877b1
Martín Javier Larré <75001420+Martin-J-Larre@users.noreply.github.com>
2021-08-18 20:39:21 -0400

    Entrega clase 5-ItemCount

--- oshii-chef-v-2/src/components/ItemListContainer.js
+++ oshii-chef-v-2/src/components/ItemListContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ItemCount from './ItemCount';
 
 const ItemsListContainer = (props) => {
     
     return (
         <div className="itemsListContainer">
            <h1 className="titulos">{props.titulo}</h1>
+           <ItemCount valorInicial={0} stock={10}/>
         </div>   
     );
 }
